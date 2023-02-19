9ebbbae19bfb29d4ff3656ee6f13788cc99da47d
Jacque Pointer <jacquepointer1@gmail.com>
2023-02-19 04:52:07 -0700

    Completed

---
+++ blog-demo/src/test/java/ca/sait/blog/blogdemo/BlogDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.sait.blog.blogdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
